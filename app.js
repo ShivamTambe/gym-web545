ab20b7b070f550844cfb069d8edf0e33e6b54f8e
unknown <shivamstambe20222@gmail.com>
2022-10-20 00:06:48 +0530

    LOGINED FILES UPDATED

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const userSchema ={
     zipcode : Number,
     email : String,
     password : String,
-    number: String
+    number: String,
+    servicee: String
 };
 
 const promoSchema={
@@ -47,6 +48,31 @@ const UserInfo = mongoose.model("UserInfo", userSchema);
 const PromoInfo = mongoose.model("PromoInfo",promoSchema);
 
 
+
+
+app.post("/loginedgym",function(req,res){
+    let servicee =  req.body.service;
+    let num = req.body.number;
+    console.log(servicee);
+    console.log(num);
+    if(servicee == "economical"){
+        GymInfo.find({ discountprice: {$lte:150, $gt:50}}).then(result =>{
+            console.log(servicee);
+            // console.log(result);
+            res.render('loginedgym',{ item : result, no : num,  s: servicee});
+        }).catch(err => console.log(err));
+    }
+    else{
+        if(servicee=="premimum"){
+            GymInfo.find({ discountprice: {$lte:500, $gt:0}}).then(result =>{
+                console.log("second");
+                res.render('loginedgym',{ item : result, no : num,  s: servicee});
+            }).catch(err => console.log(err));
+        }else{
+            res.render("nodetails");
+        }
+    }
+})
 app.get("/",function(req,res){
     GymInfo.find().then(result =>{
         // console.log(result);
@@ -75,23 +101,26 @@ app.post("/signup",function(req,res){
                 if(password == result[i].password && email == result[i].email){
                     console.log(result[i]._id);
                     let num = result[i].number;
+                    let servicee = result[i].servicee;
+                    console.log(servicee);
                     console.log(result[i].number);
                     GymInfo.find().then(resultt =>{
-                        res.render('loginedhome',{ item : resultt, no : num});
+                        res.render('loginedhome',{ no : num, s: servicee});
                     }).catch(err => console.log(err));
                     a++;
                     console.log(a);
             }
         }
     }).catch(err => console.log(err));
-    GymInfo.find().then(resultt =>{
-        console.log(a);
-        if(a==0){
-            GymInfo.find().then(resultt =>{
-                res.render('login');
-            }).catch(err => console.log(err));
-        }
-    }).catch(err => console.log(err));
+
+    // GymInfo.find().then(resultt =>{
+    //     console.log(a);
+    //     if(a==0){
+    //         GymInfo.find().then(resultt =>{
+    //             res.render('login');
+    //         }).catch(err => console.log(err));
+    //     }
+    // }).catch(err => console.log(err));
 })
 app.post("/promo",function(req,res){
     let promocodee = req.body.promocode;
@@ -99,7 +128,7 @@ app.post("/promo",function(req,res){
     PromoInfo.find({promocode:`${promocodee}`}).then(result =>{
         console.log(result);
         console.log(result[0].value);
-        res.render('promoprice',{ value1 : result[0].value});
+        res.render('promoprice',{value1 : result[0].value});
     }).catch(err => console.log(err));
 })
 
